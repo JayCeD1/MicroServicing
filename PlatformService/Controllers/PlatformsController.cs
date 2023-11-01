9ff2ab55b520e804f0da69234bea6162a48878b2
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-01 09:54:32 +0300

    post endpoint

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers
 {
@@ -24,8 +25,33 @@ namespace PlatformService.Controllers
             Console.WriteLine("--> Getting Platforms");
 
             var platformItem = repo.GetAllPlatforms();
-
+            
             return Ok(mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            Console.WriteLine("--> Getting Platform");
+
+            var platformItem = repo.GetPlatformById(id);
+            if (platformItem != null)
+            {
+                return Ok(mapper.Map<PlatformReadDto>(platformItem));
+
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platformModel = mapper.Map<Platform>(platformCreateDto);
+            repo.CreatePlatform(platformModel);
+            repo.SaveChanges();
+
+            var platformReadDto = mapper.Map<PlatformReadDto>(platformModel);
+            return CreatedAtRoute(nameof(GetPlatformById), new { platformReadDto.Id }, platformReadDto);
         }
     }
 }
