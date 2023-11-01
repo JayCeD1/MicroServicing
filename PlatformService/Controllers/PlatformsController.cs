68068782b414487858f16e9772794e330a28fae9
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-01 08:57:52 +0300

    created controller and then using DI and auto mapper

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo repo;
+        private readonly IMapper mapper;
+
+        public PlatformsController(IPlatformRepo repo, IMapper mapper)
+        {
+            this.repo = repo;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms() 
+        {
+            Console.WriteLine("--> Getting Platforms");
+
+            var platformItem = repo.GetAllPlatforms();
+
+            return Ok(mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+    }
+}
