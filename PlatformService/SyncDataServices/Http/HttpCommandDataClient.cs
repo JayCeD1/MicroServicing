5a1e52143e5b69dd775b918d69baea1a0f338e7e
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-01 22:19:56 +0300

    configuring command service

---
+++ PlatformService/SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,36 @@
+﻿using PlatformService.Dtos;
+using System.Text;
+using System.Text.Json;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient httpClient;
+        private readonly IConfiguration configuration;
+
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            this.httpClient = httpClient;
+            this.configuration = configuration;
+        }
+        public async Task SendPlatformToCommand(PlatformReadDto platform)
+        {
+            var httpContent = new StringContent(
+                JsonSerializer.Serialize(platform),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await httpClient.PostAsync($"{configuration["CommandService"]}/api/c/platforms/", httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("--> Sync Post To CommandService was Ok!");
+            }
+            else
+            {
+                Console.WriteLine("--> Sync Post To CommandService was not Ok!");
+            }
+        }
+    }
+}
