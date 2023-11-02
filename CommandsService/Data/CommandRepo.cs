b0bec37802404b7f98e725ff2c20afdee8c840ae
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-02 19:20:24 +0300

    dbcontext and repos

---
+++ CommandsService/Data/CommandRepo.cs
@@ -0,0 +1,60 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public class CommandRepo : ICommandRepo
+    {
+        private readonly AppDbContext context;
+
+        public CommandRepo(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void CreateCommand(int platformId, Command command)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+            command.PlatformId = platformId;
+            context.Commands.Add(command);
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+            context.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return context.Platforms.ToList();
+        }
+
+        public Command GetCommand(int platformId, int commandId)
+        {
+            return context.Commands.Where(c => c.Id == commandId && c.PlatformId == platformId).FirstOrDefault();
+        }
+
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+        {
+            return context.Commands.Where(c => c.PlatformId == platformId)
+                .OrderBy(c => c.Platform.Name)
+                .ToList();
+        }
+
+        public bool PlatformExists(int platformId)
+        {
+            return context.Platforms.Any(p => p.Id == platformId);
+        }
+
+        public bool SaveChnages()
+        {
+            return context.SaveChanges() >= 0;
+        }
+    }
+}
