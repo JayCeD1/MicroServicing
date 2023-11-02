542cc6d9251544ba66173d1177963f72094a4d8b
JayCeD1 <jesse.okanya@arxcess.com>
2023-11-02 19:18:26 +0300

    command service controllers

---
+++ CommandsService/Controllers/CommandsController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/platforms/{platformId}/[controller]/")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommandRepo repo;
+        private readonly IMapper mapper;
+
+        public CommandsController(ICommandRepo repo, IMapper mapper)
+        {
+            this.repo = repo;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId)
+        {
+            Console.WriteLine($"--> Hit GetCommandsForPlatform: {platformId}");
+
+            if (!repo.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+
+            var commands = repo.GetCommandsForPlatform(platformId);
+
+            return Ok(mapper.Map<IEnumerable<CommandReadDto>>(commands));
+        }
+
+        [HttpGet("{commandId}", Name = "GetCommandForPlatform")]
+        public ActionResult<CommandReadDto> GetCommandForPlatform(int platformId, int commandId)
+        {
+            Console.WriteLine($"--> Hit GetCommandForPlatform: {platformId} / {commandId}");
+
+            if (!repo.PlatformExists(platformId))
+            {
+                return NotFound();
+            }
+
+            var command = repo.GetCommand(platformId, commandId);
+
+            if (command == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(mapper.Map<CommandReadDto>(command));
+        }
+
+        [HttPost]
+        public ActionResult<CommandReadDto> CreateCommandForPlatform(int platformId, CommandCreateDto createDto)
+        {
+
+        }
+    }
+}
---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
+using CommandsService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly ICommandRepo repo;
+        private readonly IMapper mapper;
+
+        public PlatformsController(ICommandRepo repo, IMapper mapper)
+        {
+            this.repo = repo;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetAllPlatforms()
+        {
+            Console.WriteLine("--> Getting all platforms from Command Service");
+            var platformItems = repo.GetAllPlatforms();
+
+            return Ok(mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of Platform Controller");
+        }
+    }
+}
